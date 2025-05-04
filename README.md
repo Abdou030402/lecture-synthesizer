62fbfb7aff394722cdf940d5d5849499c8c2609f
Abdou030402 <a.chalghaf02@gmail.com>
2025-05-04 23:48:34 +0200

    trOCR, PaddleOCR, TesseractOCR, easyOCR and Donut test

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Bachelor Thesis: AI-Powered Lecture Synthesizer: Generating Human Like Course Delivery from Textual and Handwritten Notes
 
 # Local Development : 
-1. Activate the virtual environment (every session): .venv\Scripts\activate
+1. Navigate to directory : cd ~/lecture-synthesizer
+2. Activate the virtual environment : .venv\Scripts\activate
 
 # Note: 
 Whenever you install new packages with pip install ..., make sure to update the requirements.txt file:
---
+++ ocr/PaddleOCR.py
@@ -0,0 +1,21 @@
+from paddleocr import PaddleOCR
+from preprocessing import preprocess_image
+
+INPUT_IMAGE_PATH = "test OCR/handwritten2.png"
+PREPROCESSED_IMAGE_PATH = "test OCR/handwritten2_preprocessed.png"
+
+preprocess_image(INPUT_IMAGE_PATH, PREPROCESSED_IMAGE_PATH)
+
+ocr = PaddleOCR(use_angle_cls=True, lang='en')
+result = ocr.ocr(PREPROCESSED_IMAGE_PATH, cls=True)
+
+extracted_text = []
+for line in result[0]:
+    extracted_text.append(line[1][0])
+
+output = "\n".join(extracted_text)
+
+with open("paddleocr2_output.txt", "w", encoding="utf-8") as f:
+    f.write(output)
+
+print("[PaddleOCR] Done. Saved to paddleocr_output.txt")
